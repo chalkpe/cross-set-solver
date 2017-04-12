7a3def5904953b7bba5cdd69cd9158247758ca6c
ChalkPE <chalk@chalk.pe>
2017-04-12 10:53:57 +0900

    Better complete animation

--- app/components/Puzzle.vue
+++ app/components/Puzzle.vue
@@ -1,17 +1,18 @@
 <template lang="pug">
-  #puzzle.container(:class='puzzle.done && "done"')
+  .container
     h1.title Level {{ puzzle.level }}-{{ puzzle.stage }}
     h2.subtitle {{ puzzle.size }} × {{ puzzle.size }}
       span(v-show='puzzle.done') : Puzzle solved!
 
-    .columns.is-mobile(v-for='(row, ri) in puzzle.data')
-      .column(v-for='(cell, ci) in row'): cell(:cell='cell', :key='ci')
-      .indicator.right: .circle(:active='puzzle.matches(`row`, ri)')
+    #puzzle(:class='puzzle.done && "done"')
+      .columns.is-mobile(v-for='(row, ri) in puzzle.data')
+        .column(v-for='(cell, ci) in row'): cell(:cell='cell', :key='ci')
+        .indicator.right: .circle(:active='puzzle.matches(`row`, ri)')
 
-    .columns.is-mobile
-      .column(v-for='(_, i) in puzzle.data[0]')
-        .indicator: .circle(:active='puzzle.matches(`column`, i)')
-      .indicator.right: .circle.hidden(active)
+      .columns.is-mobile
+        .column(v-for='(_, i) in puzzle.data[0]')
+          .indicator: .circle(:active='puzzle.matches(`column`, i)')
+        .indicator.right: .circle.hidden(active)
 </template>
 
 <script>
@@ -35,13 +36,14 @@
     40%, 60%
       transform: translateX(4px)
 
+  .title, .subtitle
+    color: #777777
+    text-transform: uppercase
+
   #puzzle
     &.done
-      animation: shake 1s infinite
-
-    .title, .subtitle
-      color: #777777
-      text-transform: uppercase
+      .column
+        animation: shake 1s infinite
 
     .column
       max-width: 160px
@@ -60,7 +62,7 @@
         border-radius: 50%
         border: 0.2rem solid white
         background-color: #808080
-        transition: all 1s ease
+        transition: all 0.7s ease
 
         &[active]
           opacity: 0
