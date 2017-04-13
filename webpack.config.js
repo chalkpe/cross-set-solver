776e38203544b6ab04e4780414aba7a883afdb98
ChalkPE <chalk@chalk.pe>
2017-04-13 23:02:24 +0900

    Added docs folder

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
   entry: './app/app.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'docs')
   },
 
   plugins: [
